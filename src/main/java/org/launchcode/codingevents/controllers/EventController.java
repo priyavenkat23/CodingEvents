284f9192818451d1d5cae60fdf966cc29861180a
priyavenkat23 <shanmugapriya.gunaseelan@gmail.com>
2024-05-01 20:17:52 -0500

    static resource

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,10 +1,10 @@
 package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -14,25 +14,25 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-
     private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
     @GetMapping("create")
-    public String displayCreateEventForm() {
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
+    public String processCreateEventForm(@RequestParam String eventName) {
         events.add(eventName);
         return "redirect:/events";
     }
-
 
 }
